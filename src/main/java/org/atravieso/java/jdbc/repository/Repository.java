4f71f3f11016cd14b1da2b5f8ac6aea246424fa1
AngelTravieso <angeltraviesoc@gmail.com>
2023-07-15 11:32:51 -0400

    Repositorio CRUD de productos

---
+++ src/main/java/org/atravieso/java/jdbc/repository/Repository.java
@@ -0,0 +1,16 @@
+package org.atravieso.java.jdbc.repository;
+
+import java.util.List;
+
+public interface Repository<T> {
+
+    List<T> listar();
+
+    T porId();
+
+    // Este lo usaremos para guardar y actualizar
+    void guardar(T t);
+
+    void eliminar(Long id);
+
+}
